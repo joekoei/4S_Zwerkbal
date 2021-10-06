65d111b876f7e64fc484eb9da0a5b4d09fe73015
Joey Blommers <joeyblommers2004@gmail.com>
2021-10-06 10:22:50 +0200

    Teams push lol

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -14,7 +14,8 @@ class TeamController extends Controller
      */
     public function index()
     {
-        throw new \Exception('Niet geïmplementeerd - lijst van teams kan nog niet getoond worden.');
+        $teams = Team::all();
+        return view("teams.index")->with('teams',$teams);
     }
 
     /**
--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -1,2 +1,30 @@
+@extends('layout')
 
-<!-- hier komt een lijst van teams -->
+@section('nav')
+    <nav>
+        <a href="/">Home</a>
+        <a href="/tournaments">Toernooien</a>
+        <a href="/teams" class="active">Teams</a>
+        <a href="/players">Spelers</a>
+    </nav>
+@endsection
+
+@section('content')
+
+    <h1>Teams</h1>
+    <table class="table">
+        <tr>
+            <th>Team</th>
+            <th>Soort</th>
+            <th>Herkomst</th>
+        </tr>
+        @foreach($teams as $team)
+            <tr>
+                <td>{{ $team->name }}</td>
+                <td>{{ $team->type }}</td>
+                <td>{{$team->origin}}</td>
+            </tr>
+        @endforeach
+    </table>
+
+@endsection
